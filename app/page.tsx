0ef66b1287b6fe6f84fd9f90249ea44193c99c0a
HarshIyer <git@hi05.xyz>
2024-11-13 21:04:06 +0530

    Update npm

--- app/page.tsx
+++ app/page.tsx
@@ -230,7 +230,7 @@ export default function Home() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t dark:border-gray-800">
         <p className="text-xs text-gray-500">
-          © {new Date().getFullYear()} Cypher. All rights reserved.
+          © {new Date().getFullYear()} Harsh Iyer. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
